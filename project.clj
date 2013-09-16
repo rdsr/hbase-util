b1da5be8527c581a3a40ef556f4f5168470fa6ff
rdsr <rdsr@yahoo-inc.com>
2013-09-16 20:41:32 +0530

    logging and table configuration

--- project.clj
+++ project.clj
@@ -4,15 +4,7 @@
   :resource-paths ["conf"]
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [clj-yaml "0.4.0"]
-                 [org.apache.hbase/hbase "0.94.5.6.1302190003"]
-                 [org.apache.hadoop/hadoop-common "0.23.9"]
-                 [org.apache.zookeeper/zookeeper "3.4.5"]
-                 [org.codehaus.jackson/jackson-core-asl "1.7.1"]
-                 [org.codehaus.jackson/jackson-mapper-asl "1.7.1"]
-                 [org.codehaus.jackson/jackson-jaxrs "1.7.1"]
-                 [org.codehaus.jackson/jackson-xc "1.7.1"]
-                 [org.apache.hadoop/hadoop-common "0.23.8"
-                  :exclusions [org.codehaus.jackson/jackson-core-asl
-                               org.codehaus.jackson/jackson-mapper-asl
-                               org.codehaus.jackson/jackson-jaxrs
-                               org.codehaus.jackson/jackson-xc]]])
+                 [org.apache.hbase/hbase "0.94.10"]
+                 [org.apache.hadoop/hadoop-core "1.1.2"]
+                 [org.apache.zookeeper/zookeeper "3.4.5"]])
+  ;:repositories {"yahoo" "http://ymaven.corp.yahoo.com:9999/proximity/repository/public"})
