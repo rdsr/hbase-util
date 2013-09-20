a37cebc11948798a528687dde728d381b6cc480d
rdsr <rdsr@yahoo-inc.com>
2013-09-20 15:00:02 +0530

    adding security

--- src/clj/hbase_util/vars.clj
+++ src/clj/hbase_util/vars.clj
@@ -1,8 +1,12 @@
 (ns hbase-util.vars
+  (:require [hbase-util.util :as u])
   (:import [java.io File]
            [org.apache.hadoop.conf Configuration]
            [org.apache.hadoop.hbase HBaseConfiguration]
            [org.apache.hadoop.hbase.client HBaseAdmin]))
 
 (def conf (HBaseConfiguration/create))
+(when (u/secure? conf)
+  (u/kinit (System/getenv "grid_user") (System/getenv "grid_keytab")))
+
 (def admin (HBaseAdmin. conf))
