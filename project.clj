170d09cea745711e16588ee7656ce6a9a421631e
rdsr <rdsr.me@gmail.com>
2013-09-22 07:49:18 +0530

    code now works with secure hbase

--- project.clj
+++ project.clj
@@ -2,13 +2,13 @@
   :description "Little things to make life easier working on Hbase"
   :source-paths ["src/clj"]
   :java-source-paths ["src/jvm"]
-  :resource-paths ["conf"]
   :repl-options {:prompt (fn [_] "=> ")
                  :welcome (println "Hbase utility shell")
                  :init-ns hbase-util.core}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [clj-yaml "0.4.0"]
+  :dependencies [[clj-yaml "0.4.0"]
+                 [org.apache.hadoop/hadoop-common "0.23.8" :scope "provided"]
+                 [org.apache.hbase/hbase "0.94.5.6.1302190003" :scope "provided"]
+                 [org.apache.zookeeper/zookeeper "3.4.5" :scope "provided"]
+                 [org.clojure/tools.logging "0.2.6"]
                  [reply "0.2.1"]
-                 [org.apache.hbase/hbase "0.94.5.6.1302190003"]
-                 [org.apache.hadoop/hadoop-common "0.23.6.2.1302191452"]
-                 [org.apache.zookeeper/zookeeper "3.4.5"]])
+                 [org.clojure/clojure "1.5.1"]])
