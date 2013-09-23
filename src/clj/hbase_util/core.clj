39a2a468c532d3669fb4fa94e20d6c2560b9d974
rdsr <@rdsr.me@gmail.com>
2013-09-23 14:12:26 +0530

    delete core.clj

--- src/clj/hbase_util/core.clj
+++
@@ -1,9 +0,0 @@
-(ns hbase-util.core
-  (:require [hbase-util.table.create :as c]
-            [hbase-util.table.reset :as r]
-            [hbase-util.table.verify :as v]))
-
-;; remove this hack
-(def create c/create)
-(def reset  r/reset)
-(def verify v/verify)
